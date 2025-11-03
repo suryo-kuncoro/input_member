4e00acce2906c30334a4b9153253a4408bb1469c
suryo-kuncoro <kuncorosuryo@yahoo.com>
2025-11-03 09:12:48 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import './utils/storage'; // ← Tambahkan ini
+import './utils/storage';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
